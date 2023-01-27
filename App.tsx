c0502dfddf6f868d49182217d6f6ab895c11fd16
islanderupnorth <nhabla@gmail.com>
2023-01-27 14:49:30 +0800

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import algosdk from 'algosdk';
+
+// const token = '';
+// const server = 'https://testnet-api.algonode.cloud';
+// const port = '';
+// const client = new algosdk.Algodv2(token, server, port);
+
+// console.log('client', client);
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Howdy</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
